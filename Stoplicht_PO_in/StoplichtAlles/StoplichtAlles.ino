64c99f198f561e8aec0062522c148c32086b7739
Woutkr <wout.kruithof2003@gmail.com>
2019-03-01 21:33:47 +0100

    Stoplicht alles v2
    betere code, netter, voetgangers toegevoegd

--- Stoplicht_PO_in/StoplichtAlles/StoplichtAlles.ino
+++ Stoplicht_PO_in/StoplichtAlles/StoplichtAlles.ino
@@ -1,27 +1,37 @@
 /*Kleuren voor de stoplichten, g1 t/m g4 is groen en r1 t/m r4 is rood. Door rood en groen te mengen krijg je oranje.
 dus als je een rgb-led gebruikt kan je alle kleuren in 1 led laten zien door te mengen. 
-Als je geen rgb gebruikt is oranje zodra 2 lampjes tegelijk branden.*/
-int g1 = 9;
-int g2 = 6;
-int g3 = 7;
-int g4 = 8;
-int r1 = 2;
-int r2 = 3;
-int r3 = 4;
-int r4 = 5;
+Als je geen rgb gebruikt is oranje zodra groen en rood tegelijk branden.*/
+const int g1 = 9;
+const int g2 = 6;
+const int g3 = 7;
+const int g4 = 8;
+const int r1 = 10;
+const int r2 = 3;
+const int r3 = 4;
+const int r4 = 5;
 
 //Knopen voor auto's
-int b1 = A0;
-int b2 = A1;
-int b3 = A2;
-int b4 = A3;
+const int b1 = A0;
+const int b2 = A1;
+const int b3 = A2;
+const int b4 = A3;
 
 //Knoppen voor busbanen
-int bb1 = A4;
-int bb2 = A5;
+const int bb1 = A4;
+const int bb2 = A5;
+
+//knop voor voetgangers
+const int buttonPin = 2;
+int counter= 0;
+int buttonState = 0;
+
+//kleuren voor voetgangers
+const int vg = 11;
+const int vr = 12;
+
 
 //Hier kan je makkelijk de duur van het groene stoplicht veranderen
-int del = 1000;
+int del = 2000;
 
 void setup()
 {  
@@ -47,6 +57,13 @@ void setup()
   digitalWrite(bb1, HIGH);
   digitalWrite(bb2, HIGH);
   
+  // knop voor de voetgangers
+  pinMode(buttonPin, INPUT);
+  
+   /* we gebruiken een interrupt om zo altijd tijdens het runnen van het programma te kunnen herkennen of de knop is ingedrukt geweest, 
+   anders had je de knop ingedrukt moeten houden als voetganger wat irritant zou zijn*/
+  attachInterrupt(0, V_INT, RISING);
+  
   //Kleuren
   pinMode(g1, OUTPUT);
   pinMode(g2, OUTPUT);
@@ -56,6 +73,11 @@ void setup()
   pinMode(r2, OUTPUT);
   pinMode(r3, OUTPUT);
   pinMode(r4, OUTPUT);
+  
+  pinMode(vg, OUTPUT);
+  pinMode(vr, OUTPUT);
+  
+  Clr();
 }
 
 //Groen Links
@@ -160,10 +182,12 @@ void Clr(){
   digitalWrite(g2, LOW);
   digitalWrite(g3, LOW);
   digitalWrite(g4, LOW);
+  digitalWrite(vg, LOW);
   digitalWrite(r1, HIGH);
   digitalWrite(r2, HIGH);
   digitalWrite(r3, HIGH);
   digitalWrite(r4, HIGH);
+  digitalWrite(vr, HIGH);
 }
 
 //Dit is een loop, het gaat dus de heletijd rond, zodra het klaar is met het onderste "Actieve" code gaat het weer bovenaan beginnen.
@@ -195,7 +219,7 @@ void loop(){
   }
   
   //Rechter Stoplicht
-  if(digitalRead(b3) == LOW && digitalRead(bb1) == HIGH || digitalRead(bb2) == LOW) 
+  if(digitalRead(b3) == LOW && digitalRead(bb1) == HIGH || digitalRead(bb1) == HIGH && digitalRead(bb2) == LOW) 
   {
   GR();
     if(digitalRead(b1) == LOW && digitalRead(b3) == LOW || digitalRead(b2) == LOW && digitalRead(b3) == LOW || digitalRead(b4) == LOW && digitalRead(b3) == LOW){
@@ -219,5 +243,27 @@ void loop(){
     delay(200);  
     }
   }
+  //voetgangers
+  if (buttonState == HIGH && digitalRead(bb1) == HIGH || buttonState == HIGH && digitalRead(bb2) == HIGH){
+    digitalWrite(vr, LOW);
+    digitalWrite(vg, HIGH);
+    
+    for (counter = 0; counter < 40; ++counter) {
+      tone(13, 294, 100);
+      delay(50);
+      noTone(13);
+      delay(50);
+      buttonState = LOW;
+     }
+    Clr();
+  }
 }
-//negers
+
+//dit is voor de interrupt voor de voetgangers
+void V_INT() {
+  buttonState = HIGH;
+}
+  
+  
+  
+  
